ebf77fd80ed61d9bec98fc9fe6cef72b8c256f93
dwaynediddy <burrows.dwayne@icloud.com>
2023-02-20 21:27:41 +0100

    styled tab to bottom and small content to each tab

--- NavigationPrac/ContentView.swift
+++ NavigationPrac/ContentView.swift
@@ -10,19 +10,42 @@ import SwiftUI
 struct ContentView: View {
     @State private var path = [String]()
     @State private var selectedTab: Tab = .house
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
-        NavigationStack(path: $path) {
-            List {
-                NavigationLink ("title", value: "abc")
-                Button("navigate to xyz ") {
-                    path.append("new path xyz")
+//        NavigationStack(path: $path) {
+//            List {
+//                NavigationLink ("title", value: "abc")
+//                Button("navigate to xyz ") {
+//                    path.append("new path xyz")
+//                }
+                ZStack {
+                    VStack {
+                        TabView(selection: $selectedTab) {
+                            ForEach(Tab.allCases, id: \.rawValue) { tab in
+                                HStack {
+                                    Image(systemName: tab.rawValue)
+                                    Text("\(tab.rawValue.capitalized)")
+                                        .bold()
+                                        .animation(nil, value:  selectedTab)
+                                }
+                                .tag(tab)
+                            }
+                        }
+                    }
+                    VStack {
+                        Spacer()
+                        CustomTabBar(selectedTab: $selectedTab)
+                    }
                 }
-                CustomTabBar(selectedTab: $selectedTab)
-            }
-            .navigationDestination(for: String.self) { string in
-                Text(string)
-            }
-        }
+//            }
+//            .navigationDestination(for: String.self) { string in
+//                Text(string)
+//            }
+//        }
     }
 }
 
