3b46140a9f40917676231fc49b58b9c092ea08ea
dwaynediddy <burrows.dwayne@icloud.com>
2023-02-20 11:01:56 +0100

    Initial Commit

---
+++ NavigationPrac/NavigationPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPracApp.swift
+//  NavigationPrac
+//
+//  Created by Dwayne on 20/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
